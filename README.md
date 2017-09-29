184dcf9a6974c0de5beff6d3d2208f133c8cdaa6
Hewida <hewidaly@gmail.coma>
2017-09-29 10:57:38 -0400

    adding js samples files

---
+++ README.md
@@ -0,0 +1,3 @@
+      # js-trainning
+this repo  is only for trainning
+    
---
+++ js-sample1.js
@@ -0,0 +1,19 @@
+
+// strict mode is  activated
+
+"use strict"; 
+let user = 'John';
+let hello = 'Hello world!';
+alert(hello);
+
+let age = prompt('How old are you?',25 );
+
+alert(`You are ${age} years old!`); // You are 100 years old!
+
+let test = prompt("Test");
+let  question; 
+let result = confirm(question);
+
+
+
+⚠
